8ad2c886b0bec3ab939468109efc0cfbdae35a6e
Protheophage <protheophage@gmail.com>
2024-08-23 22:37:36 -0700

    Completely re-wrote TheBlackScorm from the ground up

--- ScriptsAndApplets/ForensicTools/TheBlackScorme.sh
+++ ScriptsAndApplets/ForensicTools/TheBlackScorme.sh
@@ -1,94 +1,65 @@
-#! /bin/bash
+#!/bin/bash
 
-#TBS - The Black Scorme (1) -THE (1) -BLACK (1) -SCORME (1)
+TheBlackScorme() {
+    local use_dc3dd=false
+    local log_dir=""
+    local boot_drive=$(lsblk -o NAME,MOUNTPOINT | grep -w '/' | awk '{print $1}')
+    local WhatIf=false
 
-#THE (1)
+    while [[ "$1" != "" ]]; do
+        case $1 in
+            --dc3dd | -3 ) use_dc3dd=true ;;
+            --log | -l ) shift; log_dir=$1 ;;
+            --whatif | -w ) WhatIf=true ;;
+            * ) echo "Invalid option: $1"; return 1 ;;
+        esac
+        shift
+    done
 
-#Unmount everything
-umount -a
+    if $WhatIf; then
+        for device in $(lsblk -dno NAME | grep -v $boot_drive); do
+            if $use_dc3dd; then
+                if [[ -n $log_dir ]]; then
+                    echo "Device $device will be wiped with dc3dd, and logged to $log_dir."
+                    return 0
+                else
+                    echo "Device $device will be wiped with dc3dd, and no logs will be created."
+                    return 0
+                fi
+            else
+                if [[ -n $log_dir ]]; then
+                    echo "Device $device will be wiped with dd, and logged to $log_dir."
+                    return 0
+                else
+                    echo "Device $device will be wiped with dd, and no logs will be created."
+                    return 0
+                fi
+            fi
+        done    
 
-#Find all devices
-fdisk -l | grep -o '/dev/sd..' > /root/Documents/mnt.txt
-fdisk -l | grep -o '/dev/sd.:' > /root/Documents/devs.txt
+    if [[ -n $log_dir ]]; then
+        mkdir -p $log_dir
+        if [[ $? -ne 0 ]]; then
+            echo "Failed to create log directory $log_dir"
+            return 1
+        fi
+    fi
 
-#Create Counters
-x=$(grep -c '/dev/sd' /root/Documents/mnt.txt)
-y=1
-z=1
-d=1
-
-#Create mount points
-while [ $d -le $x ]
-do
-mkdir /media/root/device$d
-d=`expr $d + 1`
-done
-
-echo " "
-echo " "
-
-#Mount devices
-while [ $y -le $x ]
-do
-mount --source $(head -$y /root/Documents/mnt.txt | tail -1) --target /media/root/device$z 
-y=`expr $y + 1`
-z=`expr $z + 1`
-done
-
-echo " "
-echo " "
-
-
-#BLACK (1)
-
-#Find devices to be ignored
-find /media/root -maxdepth 2 -iname 'iignoree_meee' > /root/Documents/live.txt
-
-#Create Counter
-l=$(grep -c '.' /root/Documents/live.txt)
-m=1
-
-#Identify devices to be ignored
-touch /root/Documents/flagd.txt
-while [ $m -le $l ]
-do
-findmnt -M $(head -$m /root/Documents/live.txt | tail -1 | grep -o '/media/root/device..') -o source >> /root/Documents/flagd.txt
-m=`expr $m + 1`
-done
-
-#Remove ignored devices from imaging list
-grep -vwE "$(grep -o "/dev/sd." /root/Documents/flagd.txt)" /root/Documents/devs.txt > /root/Documents/fdevs.txt
-
-#Clean up imaging list
-sed 's/://g' /root/Documents/fdevs.txt > /root/Documents/ilist.txt
-
-echo " "
-echo " "
-
-#SCORME (1)
-
-#Create counters
-e=$(grep -c '/dev/sd.' /root/Documents/ilist.txt)
-f=1
-
-#Image devices with dc3dd
-while [ $f -le $e ]
-do
-dc3dd hwipe="$(head -$f /root/Documents/ilist.txt | tail -1)" hash=md5
-f=`expr $f + 1`
-done
-
-#HOUSE CLEANING (1)
-
-#Remove text docs and  mount directories created by tbs
-	#to be more friendly with hard installed Linux
-rm -f /root/Documents/mnt.txt
-rm -f /root/Documents/devs.txt
-rm -f /root/Documents/live.txt
-rm -f /root/Documents/flagd.txt
-rm -f /root/Documents/fdevs.txt
-rm -f /root/Documents/ilist.txt
-umount -a
-rmdir /media/root/dev*
-
-#Written by Colby Connolly 2016
+    for device in $(lsblk -dno NAME | grep -v $boot_drive); do
+        if $use_dc3dd; then
+            if [[ -n $log_dir ]]; then
+                dc3dd hwipe=/dev/$device hash=sha256 log=${log_dir}/dc3dd${device}.log hashlog=${log_dir}/hash${device}.log
+            else
+                dc3dd hwipe=/dev/$device hash=sha256
+            fi
+        else
+            if [[ -n $log_dir ]]; then
+                dd if=/dev/zero of=/dev/$device bs=1M status=progress 2>&1 | tee -a ${log_dir}/dd${device}.log
+                sha256sum /dev/$device | tee -a ${log_dir}/hash${device}.log
+            else
+                dd if=/dev/zero of=/dev/$device bs=1M status=progress
+                sha256sum /dev/$device
+            fi
+        fi
+    done
+}
